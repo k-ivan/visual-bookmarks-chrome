4b46da3f7cd17539e65c75ac18d8a4cf5c6eac27
Ivan Kuzmichov <ivan.kuzmichov@gmail.com>
2023-02-18 19:12:09 +0300

    added initial state for tests

--- e2e/newtab.test.js
+++ e2e/newtab.test.js
@@ -1,6 +1,12 @@
 import { bootstrap } from './bootstrap';
 import { beforeAll, afterAll, describe, expect, it } from '@jest/globals';
 
+const DEFAULT_BOOKMARKS = [
+  { title: 'google', url: 'https://google.com' },
+  { title: 'youtube', url: 'https://youtube.com' },
+  { title: 'folder', url: '' }
+];
+
 describe('visual bookmark tests', () => {
   let extPage, browser;
 
@@ -8,6 +14,22 @@ describe('visual bookmark tests', () => {
     const context = await bootstrap({ devtools: true });
     extPage = context.extPage;
     browser = context.browser;
+
+    await extPage.waitForSelector('#add');
+    for (const bookmark of DEFAULT_BOOKMARKS) {
+      const { title, url } = bookmark;
+
+      // added some default bookmarks
+      await extPage.click('#add');
+      await extPage.type('#title', title),
+      await extPage.type('#url', url);
+      await extPage.evaluate(({ title, url }) => {
+        document.getElementById('title').value =  title;
+        document.getElementById('url').value =  url;
+      }, bookmark);
+
+      await extPage.click('#saveBookmarkBtn');
+    }
   });
 
   // for example
