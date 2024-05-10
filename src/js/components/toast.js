5b72ff1f04c0d41dbc05f79244874951c1bfd48a
Ivan Kuzmichov <ivan.kuzmichov@gmail.com>
2024-05-10 20:33:59 +0300

    #36 replaced the snackbar with multiple notifications

--- src/js/components/toast.js
+++ src/js/components/toast.js
@@ -1,92 +1,108 @@
 import { $createElement } from '../utils';
 
 const Toast = (() => {
-  let defaults = {
+  const DEFAULTS = {
     position: 'bottom-left',
     modClass: '',
     hideByClick: true,
     delay: 5000,
+    message: '',
     onClose: null
   };
 
-  let toast = null;
-  let closeBtn = null;
-  let timer = null;
-  let settings = null;
+  const containers = {
+    'top': {
+      el: createContainer('top')
+    },
+    'top-left': {
+      el: createContainer('top-left')
+    },
+    'top-right': {
+      el: createContainer('top-right')
+    },
+    'bottom': {
+      el: createContainer('bottom')
+    },
+    'bottom-left': {
+      el: createContainer('bottom-left')
+    },
+    'bottom-right': {
+      el: createContainer('bottom-right')
+    }
+  };
+
+  function init() {
+    const fragment = document.createDocumentFragment();
+    Object.keys(containers).forEach(key => fragment.append(containers[key].el));
+
+    document.body.append(fragment);
+  }
+
+  function createContainer(position) {
+    return $createElement('div', {
+      class: `toast-container toast-container${position ? `--${position}` : ''}`
+    });
+  }
 
   function show(data) {
-    if (document.getElementById('toast')) {
-      return hide(() => show(data));
-    }
+    let clickByManual = false;
+    const settings = { ...DEFAULTS };
 
-    settings = { ...defaults };
     if (typeof data === 'string') {
       settings.message = data;
     } else {
       Object.assign(settings, data);
     }
 
-    toast = $createElement('div', {
-      id: 'toast',
+    const toast = $createElement('div', {
       class: `toast toast--${settings.position}`
     }, {
       html: `<div class="toast__message">${settings.message}</div>`
     });
 
+    const hideToast = (evt) => {
+      if (clickByManual) return;
+
+      if (evt) {
+        if (!evt.target.closest('.toast__btn')) return;
+        clickByManual = true;
+      }
+
+      toast.classList.add('is-deleting');
+      toast.removeEventListener('click', hideToast);
+      setTimeout(() => {
+        toast.remove();
+        settings.onClose?.();
+      }, 250);
+    };
+
     if (settings.hideByClick) {
-      closeBtn = $createElement('button', {
+      const closeBtn = $createElement('button', {
         class: 'toast__btn',
         'aria-label': 'Close'
       }, {
         html: `<svg version="1.1" width="24" height="24" viewBox="0 0 24 24" fill="#000"><path d="M18.984 6.422l-5.578 5.578 5.578 5.578-1.406 1.406-5.578-5.578-5.578 5.578-1.406-1.406 5.578-5.578-5.578-5.578 1.406-1.406 5.578 5.578 5.578-5.578z"></path></svg>`
       });
-      closeBtn.addEventListener('click', closeHandler, { once: true });
-      toast.insertAdjacentElement('beforeend', closeBtn);
+      toast.append(closeBtn);
+      toast.addEventListener('click', hideToast);
     }
 
-    document.body.appendChild(toast);
-
-    if (settings.modClass) {
-      toast.classList.add(settings.modClass);
-    }
+    const container = containers[settings.position];
+    settings.position.startsWith('top')
+      ? container.el.prepend(toast)
+      : container.el.append(toast);
+    // container.el.append(toast)
 
     setTimeout(() => {
       toast.classList.add('toast-enter');
-    }, 20);
+    }, 16);
 
     if (settings.delay) {
-      if (timer) clearTimeout(timer);
-      timer = setTimeout(() => {
-        hide();
-      }, settings.delay);
+      setTimeout(hideToast, settings.delay);
     }
   }
 
-  function closeHandler(e) {
-    e.preventDefault();
-    hide();
-  }
-
-  function hide(fn) {
-    if (!toast) return;
-    if (timer) clearTimeout(timer);
-
-    settings.onClose && settings.onClose();
-    toast.classList.remove('toast-enter');
-
-    const handler = (e) => {
-      if (e.target !== toast) return;
-      try {
-        closeBtn?.removeEventListener('click', closeHandler);
-        toast.remove();
-      } catch (error) {}
-      toast = null;
-      fn && fn();
-    };
-    toast.addEventListener('transitionend', handler, {
-      once: true
-    });
-  }
+  init();
 
   return {
     show
