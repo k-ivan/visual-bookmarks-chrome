b2f5e1e19fc49697f008d05e81cd8abb1025ae2e
Ivan Kuzmichov <ivan.kuzmichov@gmail.com>
2024-08-11 19:37:03 +0300

    #162 added aspect-ratio instead width for bookmarks

--- src/js/components/ui.js
+++ src/js/components/ui.js
@@ -67,15 +67,12 @@ export default {
   calculateStyles() {
     const doc = document.documentElement;
     const grid = document.getElementById('bookmarks');
-    const gap = parseInt(window.getComputedStyle(doc).getPropertyValue('--grid-gap'), 10);
+    const gap = parseInt(window.getComputedStyle(doc).getPropertyValue('--grid-gap'));
     const columns = parseInt(settings.$.dial_columns);
-    const ratio = 4 / 3;
-
-    // Calculate and set container width
     const lsGridWidth = parseInt(settings.$.dial_width);
-    const gridWidth = Math.floor(window.innerWidth * (lsGridWidth / 100));
 
-    doc.style.setProperty('--container-width', `${gridWidth}px`);
+    // doc.style.setProperty('--container-width', `${gridWidth}px`);
+    doc.style.setProperty('--container-width', `${lsGridWidth}%`);
 
     // if there is at least one button and the width of the container is greater than 95
     // set container inline padding
@@ -87,7 +84,7 @@ export default {
       settings.$.services_enable
     ) && lsGridWidth >= 95
     ) {
-      const circBtnSize = parseInt(window.getComputedStyle(doc).getPropertyValue('--circ-btn-size'), 10);
+      const circBtnSize = parseInt(window.getComputedStyle(doc).getPropertyValue('--circ-btn-size'));
       // button size + small padding
       // value = left right(increased padding for the scrollbar)
       const paddingInline = `${circBtnSize + 20}px ${circBtnSize + 30}px`;
@@ -96,18 +93,7 @@ export default {
 
     // Calculate column dimensions
     const colWidth = Math.floor((grid.offsetWidth - ((columns - 1) * gap)) / columns);
-    const colHeight = Math.floor(colWidth / ratio);
-
     // if column width less than 80px do not update styles
-    if (colWidth < 80) {
-      doc.style.setProperty('--grid-column-width', '');
-      doc.style.setProperty('--grid-row-height', '');
-      doc.style.setProperty('--grid-columns', '');
-      return;
-    }
-
-    doc.style.setProperty('--grid-column-width', `${colWidth}px`);
-    doc.style.setProperty('--grid-row-height', `${colHeight}px`);
-    doc.style.setProperty('--grid-columns', columns);
+    doc.style.setProperty('--grid-columns', colWidth < 80 ? '' : columns);
   }
 };
