e3dd03b21598b309dfa29129fc1c2fe310e7d349
Ivan Kuzmichov <ivan.kuzmichov@gmail.com>
2026-07-13 18:35:39 +0300

    feat: use css custom select as a progressive enhancement

--- src/js/components/vb-select/index.js
+++ src/js/components/vb-select/index.js
@@ -5,9 +5,12 @@ class VbSelectFolders extends HTMLElement {
     this.selectNode = $createElement('select', {
       id: this.selectId,
       name: 'folder',
-      class: 'form-control',
+      class: 'vb-select-folders form-control',
       'aria-label': browser.i18n.getMessage('select_folder')
-    });
+    }, $createElement('button',
+      {},
+      $createElement('selectedcontent')
+    ));
 
     this.insertAdjacentElement('afterbegin', this.selectNode);
     this.#attachEvents();
@@ -45,7 +48,8 @@ class VbSelectFolders extends HTMLElement {
     }
 
     const html = this.#renderOptions(arr);
-    this.selectNode.innerHTML = html.join('');
+    this.selectNode.options.length  = 0;
+    this.selectNode.innerHTML += html.join('');
   }
 
   set value(value) {
@@ -66,6 +70,17 @@ class VbSelectFolders extends HTMLElement {
     return this.selectNode.disabled;
   }
 
+  get #optionTemplate() {
+    return /* html */`
+      <option{selected} value="{value}">
+        {name}
+        <span class="vb-select-badge">
+          <span>(</span>{childrenLength}<span>)</span>
+        </span>
+      </option>
+    `;
+  }
+
   #renderOptions(folders) {
     const folderId = this.parentFolderId ? this.parentFolderId : this.folderId;
     const options = [];
@@ -77,11 +92,18 @@ class VbSelectFolders extends HTMLElement {
         ) {
           let prefix = '-'.repeat(pass);
           if (pass > 0) {
-            prefix = `&nbsp;&nbsp;${prefix}` + '&nbsp;';
+            prefix = `&nbsp;${prefix}` + '&nbsp;';
           }
 
           const name = `${prefix} ${folder.title}`;
-          options.push(`<option${folder.id === folderId ? ' selected' : ''} value="${folder.id}">${name} (${folder.childrenLength})</option>`);
+          options.push(
+            this.#optionTemplate
+              .replace('{selected}', folder.id === folderId ? ' selected' : '')
+              .replace('{value}', folder.id)
+              .replace('{name}', name)
+              .replace('{childrenLength}', folder.childrenLength)
+          );
+
           if (folder.children.length) {
             processTree(folder.children, pass + 1);
           }
